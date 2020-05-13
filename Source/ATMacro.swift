89fb620a1c7718f2ba3ce479302ff32f3aafcfd1
wangws1990 <wangws1990@163.com>
2020-05-13 23:02:16 +0800

    fix

--- Source/ATMacro.swift
+++ Source/ATMacro.swift
@@ -9,15 +9,15 @@
 import Foundation
 import UIKit
 
-let SCREEN_WIDTH    :CGFloat    = UIScreen.main.bounds.size.width
-let SCREEN_HEIGHT   :CGFloat    = UIScreen.main.bounds.size.height
+public let SCREEN_WIDTH    :CGFloat    = UIScreen.main.bounds.size.width
+public let SCREEN_HEIGHT   :CGFloat    = UIScreen.main.bounds.size.height
 
-let iPhone_X        : Bool      = ATMacro.iPhone_X();//is iPhoneX
-let STATUS_BAR_HIGHT:CGFloat    = (iPhone_X ? 44: 20)//iPhoneX 44,other 20
-let NAVI_BAR_HIGHT  :CGFloat    = (iPhone_X ? 88: 64)//iPhoneX 88,other 64
-let TAB_BAR_ADDING  :CGFloat    = (iPhone_X ? 34 : 0)//iphoneX 34,other 0
+public let iPhoneX        : Bool       = ATMacro.iPhone_X();//is iPhoneX
+public let STATUS_BAR_HIGHT:CGFloat    = (iPhoneX ? 44: 20)//iPhoneX 44,other 20
+public let NAVI_BAR_HIGHT  :CGFloat    = (iPhoneX ? 88: 64)//iPhoneX 88,other 64
+public let TAB_BAR_ADDING  :CGFloat    = (iPhoneX ? 34 : 0)//iphoneX 34,other 0
 
-class ATMacro : NSObject {
+public class ATMacro : NSObject {
     class func iPhone_X() -> Bool{
         let window : UIWindow = ((UIApplication.shared.delegate?.window)!)!;
            if #available(iOS 11.0, *) {
